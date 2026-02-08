8c014aec20f40cc8413a5e08d1e978536481c66f
sirchuckalot <charleypicker@yahoo.com>
2026-02-07 23:48:55 -0800

    Delete build directory

--- build/vobj/Vmistable_top__ALL.d
+++
@@ -1,4 +0,0 @@
-Vmistable_top__ALL.o: Vmistable_top__ALL.cpp Vmistable_top.cpp \
- Vmistable_top.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vmistable_top__Syms.h \
- Vmistable_top__Slow.cpp Vmistable_top__Syms.cpp
--- build/vobj/tb.d
+++
@@ -1,2 +0,0 @@
-tb.o: /work/sim/tb.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vmistable_top.h
